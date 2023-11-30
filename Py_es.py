d87efc37bb67dd7e953a2d9b54ae7c940fe89171
Artur <fox9910@gmail.com>
2023-11-30 21:59:01 +0500

    Создание кнопки добавить и удалить

--- Py_es.py
+++ Py_es.py
@@ -13,3 +13,9 @@ note_label.pack(pady = 4)
 
 note_entry = customtkinter.CTkEntry(root) # окно для ввода заметок
 note_entry.pack(pady=3)
+
+save_button = customtkinter.CTkButton(root, text="Добавить", fg_color="blue30", command=save_button) # кнопка сохранения
+save_button.pack(pady=7)
+
+delete_button = customtkinter.CTkButton(root, text="Удалить", fg_color="red80", command=delete_button) # кнопка удаления
+delete_button.pack(pady=7)
