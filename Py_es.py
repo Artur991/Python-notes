effae50aeade12ea4d07e5957e56507dfc9df134
Artur <fox9910@gmail.com>
2023-11-30 21:43:59 +0500

    Задаем размеры нашего блокнота

--- Py_es.py
+++ Py_es.py
@@ -3,3 +3,7 @@ import customtkinter
 import sqlite3
 
 
+root = customtkinter.CTk()  # Объект класса
+root.title("Заметки")   # Заголовок
+root.geometry("400x700") # Размер окна блакнота
+root.resizable(0, 0)     # запрет редоктирования размера окна
