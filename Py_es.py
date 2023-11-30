b4db75b6cfa4084d4d1b0f41dddd7ef9a64d2395
Artur <fox9910@gmail.com>
2023-11-30 21:51:46 +0500

    Создание виджета для заметок

--- Py_es.py
+++ Py_es.py
@@ -7,3 +7,7 @@ root = customtkinter.CTk()  # Объект класса
 root.title("Заметки")   # Заголовок
 root.geometry("400x700") # Размер окна блакнота
 root.resizable(0, 0)     # запрет редоктирования размера окна
+
+note_label = customtkinter.CTkLabel(root, text= "Заметки", fg_color="green50")  # поле с заметками
+note_label.pack(pady = 4)
+
